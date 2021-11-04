03c11ce1f86fefa0aa5aa76b3c3a501d1511b3fc
guerrierlu <68223208+guerrierlu@users.noreply.github.com>
2021-11-04 13:40:47 +0100

    start cod big creature

--- StopOver2/Assets/Script/Gym_Etienne/C_CreatureNV3.cs
+++ StopOver2/Assets/Script/Gym_Etienne/C_CreatureNV3.cs
@@ -11,11 +11,29 @@ public class C_CreatureNV3 : MonoBehaviour
 
     public Transform pointCenterZone;
 
+    public float minTimeBetweenDrink;
+    public float maxTimeBetweenDrink;
+
+    public float minTimeBetweenEat;
+    public float maxTimeBetweenEat;
+
     private float distanceCreatureDestination;
+
+    private bool haveToDrink;
+    private bool haveToEat;
+
+    private float t1;
+    private float currentTimeBetweenDrink;
+
+    private float t2;
+    private float currentTimeBetweenEat;
     // Start is called before the first frame update
     void Start()
     {
+        haveToDrink = false;
+        haveToEat = false;
 
+        NewDestination();
     }
 
     // Update is called once per frame 
@@ -25,9 +43,85 @@ public class C_CreatureNV3 : MonoBehaviour
 
         distanceCreatureDestination = Vector3.Distance(_ZoneCreatureNV3.currentPointToGo.position, transform.position);
 
+
         if(distanceCreatureDestination < 0.5f)
         {
-            _ZoneCreatureNV3.MakeNewDestination();
+            if (_ZoneCreatureNV3.goToWater)
+            {
+                currentTimeBetweenDrink = Random.Range(minTimeBetweenDrink, maxTimeBetweenDrink);
+                haveToDrink = false;
+                _ZoneCreatureNV3.goToWater = false;
+            }
+
+            if (_ZoneCreatureNV3.goToEat)
+            {
+                currentTimeBetweenEat = Random.Range(minTimeBetweenEat, maxTimeBetweenEat);
+                haveToEat = false;
+                _ZoneCreatureNV3.goToEat = false;
+            }
+
+            if (haveToDrink)
+            {
+                FoundWater();
+            }else if(!haveToDrink && haveToEat)
+            {
+                FoundFood();
+            }
+
+            if(!haveToDrink && !haveToEat)
+            {
+                NewDestination();
+            }
+
         }
+
+        
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (!haveToDrink)
+        {
+            if(t1 >= 1)
+            {
+                haveToDrink = true;
+                t1 = 0;
+            }
+            else
+            {
+                t1 += Time.deltaTime / currentTimeBetweenDrink;
+            }
+
+        }
+
+        if (!haveToEat)
+        {
+            if(t2 >= 1)
+            {
+                haveToEat = true;
+                t2 = 0;
+            }
+            else
+            {
+                t2 += Time.deltaTime / currentTimeBetweenEat;
+            }
+        }
+    }
+
+    private void NewDestination()
+    {
+        _ZoneCreatureNV3.MakeNewDestination();
+    }
+
+    private void FoundFood()
+    {
+        _ZoneCreatureNV3.SelectFood();
+    }
+
+    private void FoundWater()
+    {
+        _ZoneCreatureNV3.FoundWater();
     }
 }
--- StopOver2/Assets/Script/Gym_Etienne/C_ZoneCreatureNV3.cs
+++ StopOver2/Assets/Script/Gym_Etienne/C_ZoneCreatureNV3.cs
@@ -6,12 +6,22 @@ public class C_ZoneCreatureNV3 : MonoBehaviour
 {
 
     public float radiusZone;
+    public GameObject[] foodCreatureNV3;
+    public GameObject water;
+
+    [Space]
 
     public Transform currentPointToGo;
+    public GameObject currentFoodSelected;
 
+    public bool goToWater;
+    public bool goToEat;
     // Start is called before the first frame update
     void Start()
     {
+        goToWater = false;
+        goToEat = false;
+
         MakeNewDestination();
     }
 
@@ -25,5 +35,20 @@ public class C_ZoneCreatureNV3 : MonoBehaviour
     {
         currentPointToGo.localPosition = Random.insideUnitSphere * radiusZone;
         currentPointToGo.position = new Vector3(currentPointToGo.position.x, 0.85f, currentPointToGo.position.z);
+    }
+
+    public void SelectFood()
+    {
+        goToEat = true;
+
+        currentFoodSelected = foodCreatureNV3[Random.Range(0, foodCreatureNV3.Length)];
+        currentPointToGo.position = currentFoodSelected.transform.position;
+    }
+
+    public void FoundWater()
+    {
+        goToWater = true;
+
+        currentPointToGo.position = water.transform.position;
     }
 }
