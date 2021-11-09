65fb65735ed5984a0b53cb3dd80235ec2e68897f
Etienne SANNINO <e_sannino@stu-lisaa.com>
2021-11-09 10:44:12 +0100

    New script

---
+++ StopOver2/Assets/Script/Character/C_CharacterManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_CharacterManager : MonoBehaviour
+{
+    [Header("Character Script")]
+    public C_CharacterControler _CharacterControler;
+    public C_CharacterBoost _CharacterBoost;
+    public C_CharacterInput _CharacterInput;
+    public C_CharacterFX _CharacterFX;
+    public C_CharacterPropulseur _CharacterPropulseur;
+    public C_CharacterCalculAngle _CharacterCalculAngle;
+    public C_CharacterAnime _CharacterAnime;
+
+    
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
