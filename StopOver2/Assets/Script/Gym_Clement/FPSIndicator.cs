d16610ae3c22998f9f240f7771f185fa6855171f
Clément BOUBENNE <c_boubenne@stu-lisaa.com>
2022-04-25 12:58:07 +0200

    update texture

--- StopOver2/Assets/Script/Gym_Clement/FPSIndicator.cs
+++ StopOver2/Assets/Script/Gym_Clement/FPSIndicator.cs
@@ -7,15 +7,17 @@ public class FPSIndicator : MonoBehaviour
 {
     [HideInInspector]
     public TextMeshPro TMP;
-
+    [SerializeField] private float f = 0.1f;
+    private WaitForSeconds seconds;
     private void Start()
     {
+        seconds = new WaitForSeconds(f);
         StartCoroutine(FPS());
     }
     public IEnumerator FPS()
     {
         TMP.text = (Mathf.Ceil(1 / Time.deltaTime * 10)) / 10 + " fps"; 
-        yield return new WaitForSeconds(0.1f);
+        yield return seconds;
         StartCoroutine(FPS());
     }
 }
